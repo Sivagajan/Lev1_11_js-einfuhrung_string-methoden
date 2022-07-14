14c9d4cd5e83e1307215234d208fa12f49f04835
Sivagajan <sivagajan@gmail.com>
2022-07-14 14:29:12 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+
+let x = 'Hello '
+let y = 'World<br>'
+
+let z = x+y
+document.write(z)
+
+let n = x +' ' +  y
+document.write(n)
+
+document.write('3 ' + x + ' World')
+
+let meinString = 'ich bin Erster'
+meinString += ' ich komme auf platz zwei'
+console.log(meinString)
